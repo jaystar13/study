2d7294f5f7995c31f85874bfddf2d643fd41186a
jaystar13 <astarisk2@gmail.com>
2024-02-26 21:38:53 +0900

    코틀린 강의 11

---
+++ 2024-02-21-kotlin-with-java/src/main/java/com/lannstark/lec11/Lec11Main.kt
@@ -0,0 +1,9 @@
+package com.lannstark.lec11
+
+private val a = 3
+
+fun add(a: Int, b: Int) : Int {
+    return a + b
+}
+
+open class Cat protected constructor()
---
+++ 2024-02-21-kotlin-with-java/src/main/java/com/lannstark/lec11/StringUtils.kt
@@ -0,0 +1,18 @@
+package com.lannstark.lec11
+
+fun isDirectoryPath(path: String): Boolean {
+    return path.endsWith("/")
+}
+
+fun isDirectoryPath2(path: String): Boolean {
+    return path.endsWith("/")
+}
+
+class Dog {
+    fun test1() = "test"
+
+}
+
+fun isDirectoryPath3(path: String): Boolean {
+    return path.endsWith("/")
+}
